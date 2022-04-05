a49249f6615c98bb78cf8b1f807a3115679f9670
Dmytro Prokopiuk <dproko@softserveinc.com>
2022-04-05 18:22:03 +0300

    <feat> get date from firestore

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,27 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 
-import { ProductsContext } from "../../contexts/products.context";
+import { CategoriesContext } from "../../contexts/categories.context";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
-import './shop.styles.scss';
+import "./shop.styles.scss";
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext)
-    return (
-        <div className="products-container">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product}/>
-          ))}
-        </div>
-      )
+  const { categories } = useContext(CategoriesContext);
+  return (
+    <>
+      {Object.keys(categories).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categories[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
+      ))}
+    </>
+  );
 };
 
 export default Shop;
