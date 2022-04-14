3c13414b4a799853b5e054eb493e3f3d2d7b5518
Dmytro Prokopiuk <dproko@softserveinc.com>
2022-04-14 15:33:53 +0300

    Use redux for user

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,18 +1,19 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.components';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
   return (
